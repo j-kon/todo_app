b4155c4951c546db663ab3ee9e6c8baae15bc0d7
j-kon <jaykon4me@gmail.com>
2020-07-13 04:40:53 +0100

    add assests, pubspec

---
+++ lib/controllers/firebase_controller.dart
@@ -0,0 +1,36 @@
+
+
+import 'package:todo_app/model/task_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirebaseController{
+
+  Firestore bd = Firestore.instance;
+  
+
+  FirebaseController();
+
+  saveFirebase(TarefaModel tarefa, FirebaseUser fbUser, String data)async{
+    await bd.collection("tarefa_bd").document(fbUser.uid).collection(data.replaceAll("/", "-")).add(tarefa.toMap());
+  }
+
+
+  updateTarefaTitleFirebase(FirebaseUser fbUser, String data, String selectedDoc,String tarefaTitle)async{
+    await bd.collection("tarefa_bd").document(fbUser.uid).collection(data.replaceAll("/", "-")).document(selectedDoc).updateData({"tarefa":tarefaTitle} );
+  }
+
+
+  updadeStatusFirebase(FirebaseUser fbUser, String data,String selectedDoc, bool concluido)async{
+    await bd.collection("tarefa_bd").document(fbUser.uid).collection(data.replaceAll("/", "-")).document(selectedDoc).updateData({"concluido":concluido} );            
+  }
+
+  
+  deleteTarefaFirebase(FirebaseUser fbUser,String data, String selectedDoc)async{
+    await bd.collection("tarefa_bd").document(fbUser.uid).collection(data.replaceAll("/", "-")).document(selectedDoc).delete();
+  }
+  
+
+ 
+
+}
